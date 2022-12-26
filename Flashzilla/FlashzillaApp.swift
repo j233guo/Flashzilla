f8405a3615c9329ef5964ccb624f2c117cf09e27
Jiaming Guo <j3guo@icloud.com>
2022-12-26 00:02:57 -0800

    initial commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Jiaming Guo on 2022-12-26.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
